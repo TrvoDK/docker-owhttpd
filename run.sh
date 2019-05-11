68300d543c6bd3888dac00ff3606904c71eee734
TrvoDK <33641884+TrvoDK@users.noreply.github.com>
2019-05-11 10:16:39 +0200

    Remove debug lines

--- run.sh
+++ run.sh
@@ -18,11 +18,9 @@ if [ -n "$OWSERVER" ]; then
     sed -i -r -e "s|#\[\[OWSERVER\]\]|http: server=${OWSERVER}:4304|" ${CONFIG_FILE}
 fi
 
-echo "${CONFIG_TEMPLATE}"
-cat ${CONFIG_TEMPLATE}
-
-echo "${CONFIG_FILE}"
+echo "Using ${CONFIG_FILE}"
 cat ${CONFIG_FILE}
+echo ""
 
 echo "=> Starting owhttpd ..."
 exec owhttpd -c $CONFIG_FILE --foreground --error_level=3
