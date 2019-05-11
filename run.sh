e2d651bd9630b2a4516ef552c3897a27579fb547
TrvoDK <33641884+TrvoDK@users.noreply.github.com>
2019-05-11 09:17:33 +0200

    Cleanup run script and template
    - Removed unnecessary config sections not required for OWHTTPD
    - Added proper section controlling IP address of OWSERVER

--- run.sh
+++ run.sh
@@ -4,33 +4,19 @@ CONFIG_TEMPLATE="/owfs.templ"
 CONFIG_FILE="/etc/owfs.conf"
 
 echo "=> Setting up config ..."
-if [[ -z $FAKE && -z $USB && -z $SERIAL && -z $I2C ]]; then
- echo "Missing parameters! You must set either of FAKE, USB, SERIAL, or USB as environment variables." 1>&2
+if [[ -z $OWSERVER ]]; then
+ echo "Missing parameters! You must set either of OWSERVER as environment variables." 1>&2
  exit 1
 fi
 
 cp -f $CONFIG_TEMPLATE $CONFIG_FILE
 
 # use FAKE devices
-if [ -n "$FAKE" ]; then
-    echo "Using FAKE devices: $FAKE"
-    sed -i -r -e "s|#\[\[FAKE\]\]|server: FAKE = ${FAKE}|" ${CONFIG_FILE}
-fi
-# use USB device
-if [ -n "$USB" ]; then
-    echo "Using USB device: $USB"
-    sed -i -r -e "s|#\[\[USB\]\]|server: usb = ${USB}|" ${CONFIG_FILE}
-fi
-# use Serial port
-if [ -n "$SERIAL" ]; then
-    echo "Using Serial device: $SERIAL"
-    sed -i -r -e "s|#\[\[SERIAL\]\]|server: device = ${SERIAL}|" ${CONFIG_FILE}
-fi
-# use I2C device
-if [ -n "$I2C" ]; then
-    echo "Using I2C device: $I2C"
-    sed -i -r -e "s|#\[\[I2C\]\]|server: i2c = ${I2C}|" ${CONFIG_FILE}
+if [ -n "$OWSERVER" ]; then
+    echo "Using OWSERVER address: ${OWSERVER}"
+    # ! server: server=${OWSERVER}:4304
+    sed -i -r -e "s|#\[\[OWSERVER\]\]|! server: server=${OWSERVER}:4304|" ${CONFIG_FILE}
 fi
 
-echo "=> Starting owserver ..."
+echo "=> Starting owhttpd ..."
 exec owhttpd -c $CONFIG_FILE --foreground --error_level=1
